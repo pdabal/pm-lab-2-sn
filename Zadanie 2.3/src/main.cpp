080dc132655560318cca6cc1511aad72531356e0
Damian Kaczor <damian.kaczor@student.wat.edu.pl>
2020-04-25 14:21:59 +0200

    Wykonano zadanie 2.3.1 - Arduino IRQ

--- Zadanie 2.3/src/main.cpp
+++ Zadanie 2.3/src/main.cpp
@@ -1,11 +1,27 @@
 #include <Arduino.h>
+volatile int state = LOW;
+void myISR();
+
 
 int main ()
 {
-  init();
+  init();  // inicjalizacja bib. arduino
+  pinMode(12, OUTPUT); 
+  pinMode(13, OUTPUT);
+  attachInterrupt (0, myISR, CHANGE);
+  sei();
+
   while (1)
   {
-    // kod
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
   }
 }
+
+void myISR()
+{
+  state = !state;
+  digitalWrite(12, state);
 }
