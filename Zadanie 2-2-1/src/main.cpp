00c518646b3c41d7b80b0a0a5a4df3dca3c3c787
Bartek <bartlomiej.falba@student.wat.edu.pl>
2020-04-25 14:39:08 +0200

    Zadanie 2.3.1

--- Zadanie 2-2-1/src/main.cpp
+++ Zadanie 2-2-1/src/main.cpp
@@ -1,31 +1,21 @@
-#include <avr/io.h>
-#define SOLUTION 0
-uint8_t button;
-bool state = true;
+#include <Arduino.h>
+volatile int state = LOW;
+void myISR();
 
-void togglePinD13(bool * state) { // Zmiana stanu wyjścia D13
-	PORTB = (*state << 5);
-	*state = !(*state);
-	}
-
-void delay() {
- for (uint32_t j = 0x1FFFF; j > 0; j--)
- __asm__ __volatile__("nop");
- }
-
-int main() {  
-	DDRB &= !(1 << 0); // Skasowanie PB0 (D8) - praca jako wejście
-	DDRB |= ( 1<< 5); // Ustanienie PB5 (D13) - praca jako wyjście
-	while (1)
-	{
-#if (SOLUTION == 0)	
-	button = (PINB & (1 << PINB0)); // Odczytanie stanu PB0
-	if (button == 0) { // Jeżeli przycisk puszczony - przełącz
-	 togglePinD13(&state); // Zmiana stanu PB5
-	 delay(); // PAUZA
-	}
-	else
-	 delay(); // PAUZA
-#endif
+int main() {
+   init(); // Inicjalizacja biblioteki Arduino
+   pinMode(12, OUTPUT); // Konfiguracja portu PB4 (D12) jako wyjście
+   pinMode(13, OUTPUT); // Konfiguracja portu PB5 (D13) jako wyjście
+   attachInterrupt(0, myISR, CHANGE);
+   sei();
+   while (1) {
+     digitalWrite(13, HIGH);
+     delay(500);
+     digitalWrite(13, LOW);
+     delay(500);
    }
+}
+
+void myISR() {
+  state = !state;
 }
