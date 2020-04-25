ce82e128aed5924c4b4e8833f58173be8e700de9
Adrian <adrian.sitko@student.wat.edu.pl>
2020-04-25 14:25:16 +0200

    Wykonano zadanie 2.3.1 – Arduino IRQ

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -1,6 +1,21 @@
 #include <Arduino.h>
+volatile int state = LOW;
+void myISR();
 
 int main() {
-   init();
-   while (1) { /* code */}
+   init(); // Inicjalizacja biblioteki Arduino
+   pinMode(12, OUTPUT); // Konfiguracja portu PB4 (D12) jako wyjście
+   pinMode(13, OUTPUT); // Konfiguracja portu PB5 (D13) jako wyjście
+   attachInterrupt(0, myISR, CHANGE);
+   sei();
+   while (1) {
+     digitalWrite(13, HIGH);
+     delay(500);
+     digitalWrite(13, LOW);
+     delay(500);
+   }
+}
+
+void myISR() {
+  state = !state;
 }
