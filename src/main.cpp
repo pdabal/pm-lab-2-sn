d2fbfa2c05e91e473f52db5969c99980d0c6c3aa
Hubert Zbikowski <hubert.zbikowski01@student.wat.edu.pl>
2020-04-25 14:28:02 +0200

    Wykonano zadanie 2-2-2

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,27 @@
 #include <avr/io.h>
+#define LED_LENGHT 8
+
 int main()
 {
-  uint32_t i;
-  DDRB |= (1 << 5);
+  DDRD |= 0xFF;
   while (1)
   {
-    PORTB |= (1 << 5);
-    i = 0x3ffff;
-    do
+    for (uint8_t i = 0; i < LED_LENGHT; i++)
+    {
+      PORTD = (1 << i);
+      for (uint32_t j = 0x1FFFF; j > 0; j--)
+      {
+        __asm__ __volatile__("nop");
+      }
+    }
+
+    for (uint8_t i = 1; i < (LED_LENGHT - 1); i++)
+    {
+      PORTD = (PORTD >> 1);
+      for (uint32_t j = 0x1FFFF; j > 0; j--)
     {
       __asm__ __volatile__("nop");
-    } while (i--);
-    PORTB &= !(1 << 5);
-    i = 0x3ffff;
-    do
-    {
-      __asm__ __volatile__("nop");
-    } while (i--);
+      }
+    }
   }
 }
