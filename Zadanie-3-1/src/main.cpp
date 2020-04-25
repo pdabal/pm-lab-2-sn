9d5fedfeb82e1ff043b9e535b9cd29c252549fd2
Łukasz Drozd <lukasz.drozd1@student.wat.edu.pl>
2020-04-25 14:37:14 +0200

    Wykonano zadanie dodatkowe 3.1.1 – digitalToggle

---
+++ Zadanie-3-1/src/main.cpp
@@ -0,0 +1,42 @@
+#include <avr/io.h>
+
+int main()
+{
+  DDRB |= 0x3F;
+  DDRD |= 0xFF;
+  while (1)
+  {
+    uint8_t state = 1;
+    bool isChangedState = digitalToggle(13, &state);
+  }
+}
+
+bool digitalToggle(uint8_t pin, uint8_t *state)
+{
+    int _pinport = 0;
+    if (pin > 7 && pin <= 13)
+    {
+      _pinport = pin - 8;
+      if(DDRB & (1 << _pinport))
+      { 
+        if(PORTB != (*state << _pinport))
+        {
+      	  PORTB = (*state << _pinport);
+          return true;
+        }
+      }
+    }
+    else if (pin >= 0 && pin <= 7)
+    {
+      _pinport = pin;
+      if(DDRD & (1 << _pinport))
+      { 
+        if(PORTD != (!*state << _pinport))
+        {
+      	  PORTD = (*state << _pinport);
+          return true;
+        }
+      }
+    }
+  return false;
+}
