6d1dece65c4d6e90307982f6859a268401c1045e
Łukasz Drozd <lukasz.drozd1@student.wat.edu.pl>
2020-04-25 14:40:19 +0200

    Fix zadanie 3.1

--- Zadanie-3-1/src/main.cpp
+++ Zadanie-3-1/src/main.cpp
@@ -13,17 +13,15 @@ int main()
 
 bool digitalToggle(uint8_t pin, uint8_t *state)
 {
+  
     int _pinport = 0;
     if (pin > 7 && pin <= 13)
     {
       _pinport = pin - 8;
       if(DDRB & (1 << _pinport))
       { 
-        if(PORTB != (*state << _pinport))
-        {
-      	  PORTB = (*state << _pinport);
-          return true;
-        }
+      	PORTB ^= (*state << _pinport);
+        return true;
       }
     }
     else if (pin >= 0 && pin <= 7)
@@ -31,12 +29,35 @@ bool digitalToggle(uint8_t pin, uint8_t *state)
       _pinport = pin;
       if(DDRD & (1 << _pinport))
       { 
-        if(PORTD != (!*state << _pinport))
-        {
-      	  PORTD = (*state << _pinport);
-          return true;
-        }
+      	PORTD ^= (*state << _pinport);
+        return true;
       }
     }
   return false;
 }
+
+/*bool digitalToggle(uint8_t pin, uint8_t *state)
+{
+  
+    int _pinport = 0;
+    if (pin > 7 && pin <= 13)
+    {
+      _pinport = pin - 8;
+      if(DDRB & (1 << _pinport))
+      { 
+      	PORTB = (*state << _pinport);
+        return true;
+      }
+    }
+    else if (pin >= 0 && pin <= 7)
+    {
+      _pinport = pin;
+      if(DDRD & (1 << _pinport))
+      { 
+      	PORTD = (*state << _pinport);
+        return true;
+      }
+    }
+  return false;
+}
+*/
