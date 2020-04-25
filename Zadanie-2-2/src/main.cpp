8642bea0164bf126f45fb1329be9cb8fc338caf8
Tomasz Mikołajczyk <tomasz.mikolajczyk@student.wat.edu.pl>
2020-04-25 13:09:40 +0200

    Wykonano zadanie 2-2-1 - pojedyncza dioda LED D13

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,6 +1,24 @@
 #include <Arduino.h>
 #include <avr/io.h>
 
+#define DIODA PB5   //dioda podłączona pod PB5
+
 int main(void){
-  
+  uint32_t i;
+
+  DDRB |= (1<<DIODA);   //wyjście
+
+  while(1){
+    PORTB |= (1<<DIODA);  //zapalanie 
+    i = 0x3FFFF;
+      do{
+        __asm__ __volatile__("nop");  
+      } while(i--);
+
+    PORTB &= !(1<<DIODA);  //gaszenie
+    i = 0x3FFFF;
+     do{
+       __asm__ __volatile__("nop");
+     } while(i--);
+  }
 }
