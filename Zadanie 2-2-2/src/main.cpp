eec4275dbe1da4f52188b8bcd58802595a47da49
Ewelina Rębiś <ewelina.rebis@student.wat.edu.pl>
2020-04-25 14:23:33 +0200

    Wykonano zadanie 2.2.2 - linijka świetlna D0..D7

---
+++ Zadanie 2-2-2/src/main.cpp
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#define LED_LENGTH 8
+
+int main()
+{
+  DDRD |= 0xFF; //Ustawiamy wszystkie wyprowadzenia portu D jako wyjścia
+  while (1)
+  {//Kierunek D0 -> D7
+    for (uint8_t i = 0; i < LED_LENGTH; i++)
+    {
+        PORTD = (1 << i);
+        for (uint32_t j = 0x1FFFF; j > 0; j--)
+        {
+            __asm__ __volatile__("nop");
+        }
+    }
+    //Kierunek D6 -> D1
+    for (uint8_t i = 1; i < (LED_LENGTH - 1); i++)
+    {
+        PORTD = (PORTD >> 1);
+        for (uint32_t j=0x1FFFF; j > 0; j--)
+        {
+          __asm__ __volatile__("nop");
+        }
+
+    }
+  }
+
+
+}
